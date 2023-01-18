7c20bc83ca9db49688e2aefc0c1b9f608348a6c0
Bhupender Soni <official.soni532@gmail.com>
2023-01-18 22:10:45 +0530

    Created user,product,cart,wishlist Controller

---
+++ Backend/Controller/cartpage.controller.js
@@ -0,0 +1,12 @@
+
+exports.fetchProduct = (req,res)=>{
+        
+}
+
+exports.addProduct = (req,res)=>{
+
+}
+
+exports.deletProduct = (req,res)=>{
+
+}
---
+++ Backend/Controller/user.controller.js
@@ -0,0 +1,46 @@
+const UserModel = require("../Model/user.model");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+exports.register = async (req, res) => {
+    const { email, password, gender, name } = req.body;
+    try {
+        const user = await UserModel({ email });
+        if (user.length) {
+           return res.status(403).json({ Error: "User already exsisted!" })
+        }
+        await bcrypt.hash(password, 10, async (err, hash) => {
+            if (err) return res.json({ err: err.message });
+            let newUser = new UserModel({ email, password: hash, gender, name });
+            await newUser.save();
+            res.status(201).json({ message: 'User has been created.' });
+        })
+    } catch (error) {
+        res.status(500).json({ Error: error.message })
+        console.log(error)
+    }
+}
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel({ email })
+        if (!user.length) {
+            return res.status(404).json({ message: 'Invalid Credentials' })
+        }
+        bcrypt.compare(password, user[0].password, (err, result) => {
+            if (err) return res.send({ message: err.message });
+            if (result) {
+                const token = jwt.sign(user[0].id, JWT_SECRET);
+                res.json({ token });
+            } else {
+               return res.status(403).json({ message: "Invalid Credentials" })
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ Error: error.message })
+        console.log(error)
+    }
+}
