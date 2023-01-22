7150c1d458b8e98a5aee865cd46fbde38cb040f9
Bhupender Soni <official.soni532@gmail.com>
2023-01-22 17:02:01 +0530

    created api operations

---
+++ Frontend/scripts/Api_Operations.js
@@ -0,0 +1,47 @@
+const host = 'http://localhost:3500'
+async function fetchProduct(tag){
+    let res = await fetch(`${host}/api/product/fetch/${tag}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': localStorage.getItem('token')
+        }
+    })
+    if(res.ok){
+        let data = await res.json();
+        return data;
+    } else {
+        return 'something went wrong.'
+    }
+}
+
+async function userLogin(data){
+    let res = await fetch(`${host}/api/user/login`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    if(res.ok){
+        let data = await res.json();
+        return data;
+    } return 'Something went wrong'
+}
+
+async function userRegister(data){
+    let res = await fetch(`${host}/api/user/register`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    if(res.ok){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {userLogin, userRegister, fetchProduct}
